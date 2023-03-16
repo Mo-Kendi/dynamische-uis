1cdb007645c1982b805596386bd8259b099eb804
Mohamad Kendi <99072874@mydavinci.nl>
2023-03-16 13:37:12 +0100

    dit is ruitopdracht in pyhton ik ga nu in js maken

--- module_1/ruit_in_python.py
+++ module_1/ruit_in_python.py
@@ -7,16 +7,16 @@
 num = int(input("voer getal in: "))
 
 for i in range(1, num+1, 2):
-    spatie = '-'.join(['1','2','3','4','5','6','7','8','9','10'])
+    spatie = " " * ((num-i) // 2)
     nummers = str(i) * i
     print(spatie + nummers)
 
 for i in range(num-2, 0, -2):
-    spatie = '-'.join(['1','2','3'])
+    spatie = " " * ((num-i)// 2)
     nummers = str(i) * i
     print(spatie + nummers)
 
-
+    
 # maak een string-.join en dan de list meegeven
 # spatie = '-'.join(['1','2','3'])
 # print(spatie)
